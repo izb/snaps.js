b69fec44995692dc784aba2a7f861863a587900a
Ian Beveridge <ian.beveridge@gmail.com>
2013-02-15 21:56:33 +0000

    v0.1.1 - Builds via script

--- grunt.js
+++ grunt.js
@@ -5,35 +5,58 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.loadNpmTasks('grunt-requirejs');
+    grunt.loadNpmTasks('grunt-closure-tools');
 
     /* Project config */
 
     grunt.initConfig({
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true
+            },
+            build: {
+                options:{devel:false,browser:true},
+                globals:{}
+            }
+        },
+        lint: {
+            build:  ['grunt.js', 'src/**/*.js']
+        },
         requirejs: {
             snaps: {
-                out:'lib/snap.js',
+                out:'lib/snaps.js',
                 baseUrl: 'src',
                 paths: {
                     jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min'
                 },
-                //pragmas: {
-                //    doExclude: true
-                //},
-                //optimize: 'none',
-                //skipModuleInsertion: false,
-                optimizeAllPluginResources: true,
-                findNestedDependencies: true,
-                //dir:'src',
+                optimize: 'none',
                 name: 'snaps/main'
-            },
-            snaps2: {
-                baseUrl:'src',
-                name:'snaps/main',
-                out: 'lib/snaps'
+            }
+        },
+        closureCompiler:  {
+
+            options: {
+                closureCompiler: './bin/closure-compiler/compiler.jar',
+
+                compilerOpts: {
+                    compilation_level: 'SIMPLE_OPTIMIZATIONS',
+                    warning_level: 'verbose',
+                    summary_detail_level: 3
+                },
+                js: 'lib/snaps.js',
+                output_file: 'lib/snaps.min.js'
             }
         }
-
     });
 
-    grunt.registerTask('default', 'requirejs:snaps2');
+    grunt.registerTask('default', 'lint:build requirejs:snaps closureCompiler');
 };
